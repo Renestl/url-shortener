609ff0cb09e712ad6c49dc52e0409baf23f73cac
Jennifer Currie <renestl@gmail.com>
2017-03-07 16:05:37 -0600

    added routes, starting on database

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose 		= require('mongoose');
 const validUrl		= require('valid-url');
 const shortid		= require('shortid');
 
+const config		= require("./config/database");
+
 const app = express();
 
 // port number
@@ -11,20 +13,47 @@ const port = 3000;
 // set static folder
 app.use(express.static('public'));
 
+// Connect to database
+mongoose.connect(config.database);
+
+// on connection
+mongoose.connection.on('connected', () => {
+	console.log(`Connected to database ${config.database}`);
+});
+
+// on error
+mongoose.connection.on('error', () => {
+	console.log(`Database error: ${err}`);
+});
+
+
+
+
 // index route
 app.get('/', function(req, res) {
 	// serve up homepage(index.html)
-	res.send('Help');
+	res.send('HELP ME!');
+});
+
+app.get('/new/:url(*)', function(req,res) {
+	let url = req.params.url;
+
+	// check for valid url
+	if (validUrl.isUri(url)) {
+		res.send(url);
+	} else {
+		res.send('Enter a valid url.');
+	}
 });
 
 // post url route
 app.post('/new/shorten', function(req, res) {
-
+	res.send('Shorten it');
 });
 
 // redirect user to original URL that was given
 app.get('/:shortened_id', function(req, res) {
-
+	res.send('Easter egg');	
 });
 
 
