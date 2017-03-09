955ba13c6bc9f8de5c3e808b2a0496101ae848cc
Jennifer Currie <renestl@gmail.com>
2017-03-09 02:12:24 -0600

    updated database link

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 module.exports = {
-	database: "mongodb://localhost:27017/url-short-micro",
+	// database: "mongodb://localhost:27017/url-short-micro",
+	
+	database: "mongodb://<dbuser>:<dbpassword>@ds123370.mlab.com:23370/url-short-micro",
 	// secret for token
 	secret: 'mysecret'
 }
